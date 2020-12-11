ad3c8912d34e0137587ad50957fbf25d32fa2200
Oleg Kravchuck <ok@techno-les.com>
2020-12-11 15:32:49 +0300

    added function in doctrine ascii and cast

---
+++ src/Doctrine/Functions/Ascii.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Doctrine\Functions;
+
+use Doctrine\ORM\Query\AST\Functions\FunctionNode;
+use Doctrine\ORM\Query\Lexer;
+use Doctrine\ORM\Query\Parser;
+use Doctrine\ORM\Query\SqlWalker;
+
+class Ascii extends FunctionNode
+{
+    private $expr1;
+
+    public function parse(Parser $parser)
+    {
+        $parser->match(Lexer::T_IDENTIFIER);
+        $parser->match(Lexer::T_OPEN_PARENTHESIS);
+        $this->expr1 = $parser->ArithmeticPrimary();
+        $parser->match(Lexer::T_CLOSE_PARENTHESIS);
+    }
+
+    public function getSql(SqlWalker $sqlWalker)
+    {
+        return sprintf(
+            'ASCII(%s)',
+            $this->expr1->dispatch($sqlWalker)
+        );
+    }
+}
